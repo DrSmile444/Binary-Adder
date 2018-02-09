4b95486f58dced3e46d3abc1d3e0a53c1167ccb1
DrSmile444 <drsmile444@gmail.com>
2018-02-09 21:36:06 +0200

    add functional

--- main.js
+++ main.js
@@ -6,25 +6,55 @@ function runScript(button) {
   }
 }
 
-let start = 122;
-let start2 = 124;
+let bits = 16;
+let start = -8888;
+let start2 = 29999;
 
-p( mkBinary(start, 16)
-);
+p( main(start, start2, bits) );
+
+function main(num1, num2, bits) {
+  let numMain1 = mkBinary(num1, bits),
+  numMain2 = mkBinary(num2, bits);
+
+  p(numMain1)
+  p(numMain2)
+
+  processing(numMain1, numMain2);
+}
 
 function mkBinary (number, bit) {
-  let zero = '';
-  number = number.toString(2);
+  let zero = '', sign = number < 0 ? 1 : 0;
+  number = Math.abs(number).toString(2);
 
   if(number.length < bit) {
-    for (let i = 0, n = bit-number.length; i < n; i++) {
+    for (let i = 0, n = bit-number.length-1; i < n; i++) {
       zero += '0';
     }
   }
 
-  return zero + number.split('').reverse().slice(0, bit).reverse().join("");
+  return sign + zero + number.split('').reverse().slice(0, bit).reverse().join("");
 }
 
-function adder(num1, num2) {
+function processing(num1, num2) {
+  let intervalRemainder = mkBinary(0, bits),
+  intervalSum = intervalRemainder;
 
+  p(intervalRemainder)
+  p(intervalSum)
+}
+
+p( adder('0111', '0111') )
+
+function adder(num1, num2) {
+  let result = [], interval = 0;
+  num1 = num1.split('').reverse(),
+  num2 = num2.split('').reverse();
+
+  for (let i = 0, n = num1.length; i < n; i++) {
+    let calc = +num1[i] + +num2[i] + interval;
+    result.push(calc % 2);
+    interval = calc > 1 ? 1 : 0;
+  }
+
+  return result.reverse().join('');
 }
